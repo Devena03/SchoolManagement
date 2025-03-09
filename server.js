9618b742f9567c02b693b8a2b18a6f188d8bc881
Devena03 <140866409+Devena03@users.noreply.github.com>
2025-03-09 13:38:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -69,5 +69,5 @@ app.get('/listSchools', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
